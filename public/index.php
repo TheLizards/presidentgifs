196442fc1518148a48083c7b0270b966cf451396
TheLizards <jdrummera10@gmail.com>
2016-03-12 22:37:27 -0500

    Forge is working

--- public/index.php
+++ public/index.php
@@ -1,17 +1,3 @@
 <?php
 
-echo 'Hello world';
-
-echo 'Hello World 2';
-
-echo 'Hello World 3';
-
-echo 'Hello World 4';
-
-echo 'Hello World 5';
-
-echo 'Hello World 6';
-
-echo 'Hello World 7';
-
-echo 'Hello World 7';
+echo 'Forge is working';
